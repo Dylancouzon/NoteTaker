8f1a84dd17543ed8ef7501e3477553bbd6b23000
Dylancouzon <dylancouzon@gmail.com>
2021-04-22 17:34:15 -0700

    Bug fix on the IDs

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+// Express values
 const app = express();
-
 const PORT = process.env.PORT || 3000;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
